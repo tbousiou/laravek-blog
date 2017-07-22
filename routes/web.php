cbac734a1843d7f78300ab99488f53f2535d1de6
Teo Bous <tbousiou@gmail.com>
2017-07-22 14:16:10 +0300

    Fix routes posts/create

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 Route::get('/', 'PostsController@index');
 
-Route::get('/posts/{post}', 'PostsController@show');
-
 Route::get('/posts/create', 'PostsController@create');
+
+Route::get('/posts/{post}', 'PostsController@show');
 
 Route::post('/posts', 'PostsController@store');
